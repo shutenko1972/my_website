821043312a278eac088b38b4398ab0ca73c8cdbf
shutenko1972 <111730495+shutenko1972@users.noreply.github.com>
2024-12-13 23:08:48 +0400

    Добавлено логирование и обновлен wsgi.py

--- app.py
+++ app.py
@@ -1,37 +1,49 @@
 from flask import Flask, render_template
+import logging
+
+# Настройка логирования
+logging.basicConfig(filename='app.log', level=logging.DEBUG)
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    app.logger.debug('Index page accessed')
     return render_template('index.html')
 
 @app.route('/section1')
 def section1():
+    app.logger.debug('Section 1 page accessed')
     return render_template('section1.html')
 
 @app.route('/section2')
 def section2():
+    app.logger.debug('Section 2 page accessed')
     return render_template('section2.html')
 
 @app.route('/section3')
 def section3():
+    app.logger.debug('Section 3 page accessed')
     return render_template('section3.html')
 
 @app.route('/section4')
 def section4():
+    app.logger.debug('Section 4 page accessed')
     return render_template('section4.html')
 
 @app.route('/section5')
 def section5():
+    app.logger.debug('Section 5 page accessed')
     return render_template('section5.html')
 
 @app.route('/about')
 def about():
+    app.logger.debug('About page accessed')
     return render_template('about.html')
 
 @app.route('/hello/<name>')
 def hello(name):
+    app.logger.debug(f'Hello page accessed with name: {name}')
     return render_template('hello.html', name=name)
 
 if __name__ == '__main__':
--- wsgi.py
+++ wsgi.py
@@ -2,9 +2,9 @@ import sys
 import os
 
 # add your project directory to the sys.path
-project_home = os.path.expanduser('~/my_website')
+project_home = '/home/Vitaly72/my_website'
 if project_home not in sys.path:
-    sys.path.append(project_home)
+    sys.path = [project_home] + sys.path
 
 # import flask app but need to call it "application" for WSGI to work
 from app import app as application  # noqa
