8cd477911a790d183b89411335d83291650caf43
shutenko1972 <111730495+shutenko1972@users.noreply.github.com>
2024-12-14 09:39:47 +0400

    Обновлен wsgi.py с установкой рабочей директории

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,10 @@ import os
 # add your project directory to the sys.path
 project_home = '/home/Vitaly72/my_website'
 if project_home not in sys.path:
-    sys.path = [project_home] + sys.path
+    sys.path.append(project_home)
+
+# Установите рабочую директорию
+os.chdir(project_home)
 
 # import flask app but need to call it "application" for WSGI to work
 from app import app as application  # noqa
